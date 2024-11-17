32f21a70e63e78360859a204f11d1756ac54e9ef
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-11-17 21:11:07 +0100

    minor change

--- auth.js
+++ auth.js
@@ -1,5 +1,4 @@
 import { LoginProvider, RegisterProvider } from '@/app/auth/CrendentialsProvider';
-import "@/app/lib/zodErrors";
 import { authConfig } from '@/app/auth/auth.config';
 import NextAuth from 'next-auth';
 import Github from "next-auth/providers/github";
