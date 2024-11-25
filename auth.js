155906b30868d68fe99202c1cfd9e1cc3dff468c
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-11-25 01:38:42 +0100

    minor changes

--- auth.js
+++ auth.js
@@ -1,15 +1,15 @@
 import { LoginProvider, RegisterProvider } from '@/app/auth/CrendentialsProvider';
 import { authConfig } from '@/auth.config';
 import NextAuth from 'next-auth';
-import Github from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 
 export const { auth, signIn, signOut, handlers } = NextAuth({
     ...authConfig,
     providers: [
         LoginProvider,
         RegisterProvider,
-        Github,
-        Google
+        GithubProvider,
+        GoogleProvider
     ],
 });
