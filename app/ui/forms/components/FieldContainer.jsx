8ef9ad33066a5979e529df48704846eeab52c3bb
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-11-27 06:47:31 +0100

    style fieldcontainer

--- app/ui/forms/components/FieldContainer.jsx
+++ app/ui/forms/components/FieldContainer.jsx
@@ -1,8 +1,8 @@
 import Stack from '@mui/material/Stack';
 
-export default function FieldContainer({margin,...props}) {
+export default function FieldContainer({ margin, spacing = 2, ...props }) {
     return (
-        <Stack spacing={2} sx={{ alignItems: "start",my:margin&&2 }} {...props}>
+        <Stack spacing={spacing} sx={{ alignItems: "start", my: margin && 2, "& > .MuiDivider-root":{width:"100%"} }} {...props}>
             {props.children}
         </Stack>
     )
