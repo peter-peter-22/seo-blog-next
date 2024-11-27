2e11cd1ef554973d06072c48bc60d5ff9a39d983
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-11-27 07:04:11 +0100

    make login secure

--- app/auth/CrendentialsProvider.js
+++ app/auth/CrendentialsProvider.js
@@ -9,8 +9,10 @@ export const LoginProvider = Credentials({
     async authorize(credentials) {
         const parsedCredentials = LoginSchema.parse(credentials);
         const { email, password } = parsedCredentials;
-        const user = await prisma.user.findFirst({ where: { email } });
+        const user = await prisma.user.findUnique({ where: { email } });
         if (!user) return null;
+        if (!password)
+            throw new Error("This email address has no password registered.")
         const passwordsMatch = await bcrypt.compare(password, user.password);
         if (!passwordsMatch)
             return;
