3d88b671b39847708050c1e72194744948c75f50
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-11-29 00:41:30 +0100

    fix bug with placeholder

--- app/ui/editor/components/modules/placeholder.jsx
+++ app/ui/editor/components/modules/placeholder.jsx
@@ -1,10 +1,5 @@
 export default function placeholder() {
     return {
         placeholder: "Type something",
-        renderPlaceholder: ({ children, attributes }) => (
-            <div {...attributes} >
-                <p style={{margin:0}}>{children}</p>
-            </div>
-        )
     }
 }
