8edf2aaf24add0ffe45bfeb46f99839ae22c41fd
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-12-03 06:56:41 +0100

    style

--- app/(pages)/(multiple columns)/browse/page.jsx
+++ app/(pages)/(multiple columns)/browse/page.jsx
@@ -44,7 +44,7 @@ export default async function Page({ searchParams }) {
                     <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
                         {articles.map((article, i) => (
                             <Grid key={i} size={{ xs: 2, sm: 4, md: 4 }}>
-                                <Card sx={{ height: "100%" }}>
+                                <Card >
                                     <CardActionArea href={`/articles/${article.id}`} sx={{ height: "100%" }}>
                                         <CardContent>
 
