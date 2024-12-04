5bea18ecb635e89da96c93abfe348debb2a8f8cd
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-12-04 01:50:25 +0100

    fix profile

--- app/(pages)/(single column)/authors/[id]/page.jsx
+++ app/(pages)/(single column)/authors/[id]/page.jsx
@@ -4,6 +4,6 @@ import { auth } from "@/auth";
 export default async function Page(props) {
     const { id } = await props.params;
     const session = await auth();
-    const isMe = session.user.id == id;
+    const isMe = session?.user?.id === id;
     return <ProfileContainer userId={id} isMe={isMe} />
 }
