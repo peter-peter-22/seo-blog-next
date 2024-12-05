5d6e7a6c8ed7af1dc0ab37948d9454d9b2b9b590
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-12-05 05:30:10 +0100

    cleanup

--- app/(pages)/(multiple columns)/browse/BrowserLayout.jsx
+++ app/(pages)/(multiple columns)/browse/BrowserLayout.jsx
@@ -2,53 +2,12 @@
 
 import Filters from "@/app/(pages)/(multiple columns)/browse/Filters";
 import BasicDrawerButton from "@/app/ui/layout/BasicDrawerButton";
-import { MultipleColumnsAssymetric } from "@/app/ui/layout/Layouts";
-import { SideDrawer } from "@/app/ui/layout/ResponsiveMultiColumns";
+import { ResponsiveLayout } from "@/app/ui/layout/ResponsiveMultiColumns";
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
-import NoSsr from "@mui/material/NoSsr";
-import Box from "@mui/material/Box";
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import Card from '@mui/material/Card';
 import DisplayArticles from "./DisplayArticles";
 import People from './People';
-import { ResponsiveLayout } from "@/app/ui/layout/ResponsiveMultiColumns";
-import Card from '@mui/material/Card';
-
-//export default function BrowserLayout({ query, searchParams }) {
-//    const theme = useTheme();
-//    const showLeft = useMediaQuery(theme.breakpoints.up("md"));
-//    const showRight = useMediaQuery(theme.breakpoints.up("lg"));
-//
-//    const Main = <DisplayArticles {...query} {...{ searchParams }} />
-//    const Left = <Filters defaultValues={searchParams} />
-//    const Right = <People articles={query.articles} />
-//
-//    return <>
-//        <MultipleColumnsAssymetric
-//            Left={
-//                <Box sx={theme => ({
-//                    [theme.breakpoints.down("md")]: { display: "none" }
-//                })}>
-//                    {Left}
-//                </Box>
-//            }
-//
-//            Right={
-//                <Box sx={theme => ({
-//                    [theme.breakpoints.down("lg")]: { display: "none" }
-//                })}>
-//                    {Right}
-//                </Box>
-//            }
-//            Main={Main}
-//        />
-//        <NoSsr>
-//            {!showLeft && <SideDrawer anchor="left" Content={Left} ToggleButton={BasicDrawerButton({ Icon: <FilterAltIcon /> })} />}
-//            {!showRight && <SideDrawer anchor="right" Content={Right} ToggleButton={BasicDrawerButton({ Icon: <PeopleAltIcon /> })} />}
-//        </NoSsr>
-//    </>
-//}
 
 export default function BrowserLayout({ query, searchParams }) {
     const leftBreakpoint = "md";
