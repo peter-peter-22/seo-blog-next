9060c668bba66fd8d6f285b859298f61c691f606
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-12-06 07:40:17 +0100

    refractor topic trigger

--- prisma/migrations/20241203195051_add_topic_triggers/migration.sql
+++ prisma/migrations/20241203195051_add_topic_triggers/migration.sql
@@ -2,8 +2,24 @@ CREATE OR REPLACE FUNCTION recalculate_tag_counts(tags TEXT[])
 RETURNS VOID AS $$
 DECLARE
     tag TEXT;
+    new_count INT;
 BEGIN
     FOREACH tag IN ARRAY tags LOOP
+        -- calculate the count
+        new_count:=(SELECT COUNT(*) FROM "Article" WHERE tag = ANY("Article".tags));
+
+        IF new_count > 0 THEN
+            -- if the count is not 0, insert or update
+            INSERT INTO "Topic" (name, count)
+                VALUES (tag, new_count)
+            ON CONFLICT (name)
+            DO UPDATE SET
+                count=new_count;
+        ELSE
+            -- if the count is zero, delete
+            DELETE FROM "Topic" WHERE name = tag AND count = 0;
+        END IF;
+
         -- Update the count for the tag
         UPDATE "Topic"
         SET count = (SELECT COUNT(*) FROM "Article" WHERE "Article".tags @> ARRAY[tag])
