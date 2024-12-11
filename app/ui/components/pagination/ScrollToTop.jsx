f82226f553f84371b7724e7c7769a08abcb2af9f
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-12-11 05:05:20 +0100

    fix unintended scroll

--- app/ui/components/pagination/ScrollToTop.jsx
+++ app/ui/components/pagination/ScrollToTop.jsx
@@ -1,11 +1,14 @@
 "use client"
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { usePathname, useSearchParams } from "next/navigation";
 
 export default function ScrollToTop() {
     const pathname = usePathname()
     const params = useSearchParams();
+    const [first, setFirst] = useState(true);
     useEffect(() => {
+        if (first)//prevent scrolling to the top after the page was loaded for the first time
+            return setFirst(false);
         window.scrollTo({ top: 0 });
     }, [pathname, params])
 }
