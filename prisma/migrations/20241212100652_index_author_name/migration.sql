31339c719b90dd375843a6c2c379ed0035ca8115
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-12-12 14:51:30 +0100

    delete user search index

--- prisma/migrations/20241212100652_index_author_name/migration.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "User" ADD COLUMN search_vector tsvector GENERATED ALWAYS AS (
-    to_tsvector('english', name)
-) STORED;
-
-CREATE INDEX user_name_search_idx ON "User" USING gin (search_vector);
