48b1a906b0477aa29f60afa19f45ed025d0f7d74
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-12-24 23:59:03 +0100

    make description min lenght shorter

--- app/ui/forms/schemas/ArticleSchema.js
+++ app/ui/forms/schemas/ArticleSchema.js
@@ -4,7 +4,7 @@ import { lowerCaseString } from './fields/searchText';
 export const UpdateArticleSchema = z.object({
   content: z.array(z.object({}).passthrough()),
   title: z.string().min(10).max(40),
-  description: z.string().min(70).max(170),
+  description: z.string().min(50).max(170),
   //if 0 tags are provided and the array is null prisma throws an error. make sure the array is not null
   tags: z.array(lowerCaseString).min(1).max(20).catch([]),
   id: z.string()
