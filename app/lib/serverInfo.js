370dca492bf6274ba00e65c2ee44f034c659d81e
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-12-24 12:46:28 +0100

    refractor

--- app/lib/serverInfo.js
+++ app/lib/serverInfo.js
@@ -1,7 +1,7 @@
 function getUrl() {
     return process.env.NODE_ENV === "production" ?
         `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-        : process.env.DEV_URL
+        : "http://localhost:3000"
 }
 
 export const baseUrl = getUrl();
