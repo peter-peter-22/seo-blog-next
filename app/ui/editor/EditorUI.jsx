b581eaa37f01237c540f29099611f863ab0e5d57
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-12-24 21:44:31 +0100

    fix buttons on editor

--- app/ui/editor/EditorUI.jsx
+++ app/ui/editor/EditorUI.jsx
@@ -1,10 +1,10 @@
 import IconButton from '@mui/material/IconButton';
 import Tooltip from "@mui/material/Tooltip";
 
-export function MenuButton({ active, children, title }) {
+export function MenuButton({ active, children, title, ...props }) {
     return (
         <Tooltip title={title}>
-            <IconButton color={active ? "primary" : undefined} >
+            <IconButton color={active ? "primary" : undefined} {...props}>
                 {children}
             </IconButton>
         </Tooltip>
