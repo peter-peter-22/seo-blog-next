e443b36fb08ef1884b3107379a64625a42fa5f1a
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-12-25 11:53:54 +0100

    add route change loading indicator

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,6 +9,7 @@ import ScrollToTop from '@/app/ui/components/pagination/ScrollToTop';
 import CssBaseline from "@mui/material/CssBaseline";
 import AuthProvider from './ui/layout/AuthProvider';
 import NotificationProvider from './ui/layout/NotificationProvider';
+import NextTopLoader from 'nextjs-toploader';
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
@@ -22,6 +23,7 @@ export default async function RootLayout({ children }) {
     <html lang="en">
       <body className={roboto.variable}>
         <ScrollToTop />
+        <NextTopLoader color="white" shadow={false} showSpinner={false} />
         <AppRouterCacheProvider>
           <AuthProvider>
             <NotificationProvider>
