425862096cf5992da25b32c9797f3f4522c3894b
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-12-26 01:15:17 +0100

    fix baseurl

--- app/lib/serverInfo.js
+++ app/lib/serverInfo.js
@@ -1,6 +1,6 @@
 function getUrl() {
     return process.env.NODE_ENV === "production" ?
-        `https://${process.env.BASE_URL}`
+        process.env.BASE_URL
         : "http://localhost:3000"
 }
 
