fad521f54f19d2f8c2ada8d0b87fe006645ced9a
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-12-28 20:48:15 +0100

    fix button contrast

--- app/ui/layout/BasicDrawerButton.jsx
+++ app/ui/layout/BasicDrawerButton.jsx
@@ -6,7 +6,7 @@ import Zoom from '@mui/material/Zoom';
 export default function BasicDrawerButton({ Icon, ...props }) {
     const Component = ({ toggle, visible }) => (
         <Zoom in={visible} >
-            <Fab onClick={toggle} sx={{ m: 1, }} {...props}>
+            <Fab onClick={toggle} sx={{ m: 1 }} {...props} color="primary">
                 {Icon}
             </Fab>
         </Zoom>
