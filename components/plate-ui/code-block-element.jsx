9d9fb79140d465d8d33d958abef42a78d709a676
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2024-12-31 22:51:00 +0100

    add code editor

--- components/plate-ui/code-block-element.jsx
+++ components/plate-ui/code-block-element.jsx
@@ -3,7 +3,10 @@ import {
 } from '@udecode/plate-common';
 import { PlateElement, useEditorRef } from '@udecode/plate-common/react';
 import { ReactEditor } from 'slate-react';
-
+import React, { useState } from 'react';
+import CodeEditor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import "@/app/css/prism-vsc-dark-plus.css";
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
 
@@ -14,19 +17,26 @@ export function CodeBlockElement({
 }) {
     const editor = useEditorRef();
 
-    function onChange(e) {
-        const value = e.target.value;
+    function onChange(value) {
         const path = ReactEditor.findPath(editor, props.element); // Find the path of the node
-        setNodes(editor, { value }, { at: path });
+        setNodes(editor, { ...props.element, value }, { at: path });
     }
 
     return (
-        <PlateElement asChild className={className} {...props} contentEditable={false} >
-            <textarea
-                value={props.element.value}
-                onChange={onChange}
-                style={{ width: "100%",height:500 }}
-            />
+        <PlateElement asChild className={className} {...props}  >
+            <div contentEditable={false}>
+                {children}
+                <CodeEditor
+                    value={props.element.value}
+                    onValueChange={onChange}
+                    highlight={(code) => highlight(code, languages.js)}
+                    padding={10}
+                    style={{
+                        fontFamily: '"Fira code", "Fira Mono", monospace',
+                        fontSize: 12,
+                    }}
+                />
+            </div>
         </PlateElement>
     );
 }
