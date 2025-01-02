da4381f5b059323210ab536ce074e79bdcbcecae
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-01-03 00:53:31 +0100

    add code syntax selector

--- components/plate-ui/code-block-element.jsx
+++ components/plate-ui/code-block-element.jsx
@@ -1,13 +1,12 @@
 import "@/app/css/prism-vsc-dark-plus.css";
 import Box from '@mui/material/Box';
-import { PlateElement, setNode, useEditorRef, useElement } from '@udecode/plate-common/react';
-import { highlight, languages } from 'prismjs/components/prism-core';
-import CodeEditor from 'react-simple-code-editor';
-
-//must be imported after prism-core
-import 'prismjs/components/prism-clike';
-import 'prismjs/components/prism-javascript';
+import { cn } from '@udecode/cn';
+import { useCodeBlockElementState } from '@udecode/plate-code-block/react';
+import { PlateElement, setNode, useEditorRef } from '@udecode/plate-common/react';
+import { highlight } from "prismjs";
 import { useState } from "react";
+import CodeEditor from 'react-simple-code-editor';
+import { CodeBlockCombobox, Prism } from "./code-block-combo-box";
 
 export function CodeBlockElement({
     className,
@@ -15,8 +14,9 @@ export function CodeBlockElement({
     ...props
 }) {
     const editor = useEditorRef();
-    const element = useElement();
+    const { element } = props;
     const [value, setValue] = useState(element.value);
+    const state = useCodeBlockElementState({ element });
 
     function onChange(value) {
         //const path = ReactEditor.findPath(editor, props.element); 
@@ -26,37 +26,42 @@ export function CodeBlockElement({
     }
 
     return (
-        <PlateElement asChild className={className} {...props}  >
-            <Box
-                contentEditable={false}
-                component="pre"
-                className={`language-${"javascript"}`}
-                sx={theme => ({
-                    borderWidth: 2,
-                    borderStyle: "solid",
-                    borderColor: "transparent",
-                    transition: theme.transitions.create(['all'], {
-                        duration: theme.transitions.duration.shortest,
-                    }),
-                    borderRadius: 1,
-                    my: 2,
-                    "& textarea": {
-                        outlineWidth: 0
-                    },
-                    "&:focus-within": {
-                        borderColor: theme.palette.primary.light,
-                    },
-                })}>
-                <CodeEditor
-                    value={value}
-                    onValueChange={onChange}
-                    highlight={(code) => highlight(code, languages.js)}
-                    style={{
-                        fontFamily: 'var(--roboto)',
-                        fontSize: 12,
-                        backgroundColor: "revert-layer"
-                    }}
-                />
+        <PlateElement
+            asChild
+            className={className}
+            {...props}
+        >
+            <Box sx={{ position: "relative" }}>
+                <Box
+                    contentEditable={false}
+                    component="pre"
+                    className={state.className || "language-text"}
+                    sx={theme => ({
+                        borderWidth: 2,
+                        borderStyle: "solid",
+                        borderColor: "transparent",
+                        transition: theme.transitions.create(['all'], {
+                            duration: theme.transitions.duration.shortest,
+                        }),
+                        borderRadius: 1,
+                        my: 2,
+                        pt: "25px !important",
+                        "& textarea": {
+                            outlineWidth: 0
+                        },
+                        "&:focus-within": {
+                            borderColor: theme.palette.primary.light,
+                        },
+                    })}>
+                    <CodeEditor
+                        value={value}
+                        onValueChange={onChange}
+                        highlight={(code) => highlight(code, Prism.languages[element.lang ?? "text"])}
+                    />
+                </Box>
+                <Box sx={{ position: "absolute", right: 5, top: 3 }}>
+                    <CodeBlockCombobox />
+                </Box>
             </Box>
         </PlateElement>
     );
