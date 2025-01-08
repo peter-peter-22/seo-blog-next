4216c514e1b0aa34c97f1ee1087f22bbc67d8e11
Magyar-Peter-22 <81881238+peter-peter-22@users.noreply.github.com>
2025-01-08 03:25:05 +0100

    add code label for static code block

--- components/plate-ui/code-block-element-static.jsx
+++ components/plate-ui/code-block-element-static.jsx
@@ -2,6 +2,8 @@ import { cn } from '@udecode/cn';
 import { PlateElement } from '@udecode/plate-common/react';
 import { CodeBlockCopyButton } from "./code-block-copy-button";
 import { Prism } from "./code-block-combo-box";
+import Chip from '@mui/material/Chip';
+import CodeIcon from '@mui/icons-material/Code';
 
 export const CodeBlockElementStatic = ({
   children,
@@ -18,14 +20,17 @@ export const CodeBlockElementStatic = ({
 
   return (
     <PlateElement className={cn(className, 'py-1')} {...props}>
-      <div style={{ position: "relative" }} >
-        <pre
-          className={cn("overflow-x-auto rounded-md bg-muted px-6 py-8 font-mono text-sm leading-[normal] [tab-size:2]", state.className)}
-          style={{ maxHeight: 400, scrollbarWidth: "none", borderRadius: 5 }}
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-        <div style={{ position: "absolute", top: 5, right: 5 }}>
-          <CodeBlockCopyButton value={element.value} />
+      <div>
+        <Chip label={element.lang ?? "Text"} icon={<CodeIcon />} size="small" />
+        <div style={{ position: "relative" }} >
+          <pre
+            className={cn("overflow-x-auto rounded-md bg-muted px-6 py-8 font-mono text-sm leading-[normal] [tab-size:2]", state.className)}
+            style={{ maxHeight: 400, scrollbarWidth: "none", borderRadius: 5 }}
+            dangerouslySetInnerHTML={{ __html: html }}
+          />
+          <div style={{ position: "absolute", top: 5, right: 5 }}>
+            <CodeBlockCopyButton value={element.value} />
+          </div>
         </div>
       </div>
     </PlateElement>
